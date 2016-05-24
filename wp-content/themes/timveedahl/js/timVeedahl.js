ce4f8a884e26c89640676aa779e1a1852716e4c6
tim_veedahl <tveedahl@gmail.com>
2016-05-24 14:20:56 -0500

    Added storefront and storefront-child themes

--- wp-content/themes/timveedahl/js/timVeedahl.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('.navbar-toggle').click(function(){
-		if ($(".navbar-toggle").attr("aria-expanded") == "false") {
-		    $('.site-content').css("padding-top", "170px");
-		} else {
-		    $('.site-content').css("padding-top", "0");
-		}
-	});
-});
