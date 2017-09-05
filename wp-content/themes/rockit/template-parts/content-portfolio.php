06b155ac712f839cdf511eacb1ea3482fddad5b9
Tim Veedahl <tveedahl@gmail.com>
2017-09-05 08:14:28 -0500

    RockIt front and about page updates

--- wp-content/themes/rockit/template-parts/content-portfolio.php
+++ wp-content/themes/rockit/template-parts/content-portfolio.php
@@ -10,6 +10,11 @@
 ?>
 
 <?php
+    echo '<div class="row">';
+        echo '<div col-lg-6>';
+            echo '<h1 class="text-center">Portfolio</h1>';
+        echo '</div>';
+    echo '</div>';
     /**
      * Checks if portfolio category exists, and creates it otherwise
      */
