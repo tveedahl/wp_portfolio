6ad376a0673f423ce4c9454352b986137fb6a4df
Tim Veedahl <tveedahl@gmail.com>
2017-09-06 08:40:23 -0500

    timveedahl theme updates

--- wp-content/themes/timveedahl/functions.php
+++ wp-content/themes/timveedahl/functions.php
@@ -191,7 +191,7 @@ add_action('admin_notices', 'timveedahl_showAdminMessages');
 
 /* Customize More Links */
 function modify_read_more_link() {
-    return '<a class="more-link" href="' . get_permalink() . '">more...</a>';
+    return '<a class="more-link" href="' . get_permalink() . '">(more...)</a>';
 }
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 
