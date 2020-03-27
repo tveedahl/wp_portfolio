049bc922fefaff7206b093250cbf91c70d24d82f
Tim Veedahl <timveedahl@gmail.com>
2020-03-27 07:08:07 -0500

    Updated navbar brand and corresponding scss

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'timveeda_wordpress446');
+define('DB_NAME', 'timveedahl_03262020');
 
 /** MySQL database username */
 define('DB_USER', 'sirommit');
